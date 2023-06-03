03dae3897dfe28b07e3edbaacb98aa5d6081ebd8
Рычков Кирилл <kirillkirill2016@mail.ru>
2023-06-04 00:07:14 +0300

    deploy

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -106,7 +106,7 @@ exports.getProducts = async (req, res, next) => {
             products[i]["Jpg"] = productImage.img
             console.log(products[i])
             // productIngredients = products[i].Description.replaceAll(';', ',').split(',');
-            console.log(productIngredients)
+            // console.log(productIngredients)
             console.log(currentUser)
             const answer =  validateProduct(currentUser, productIngredients);
             console.log(answer)
